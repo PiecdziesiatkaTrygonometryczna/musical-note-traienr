242df35197e319e4ba5607b486c6d2ec0bdfe632
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-03-31 22:22:12 +0200

    history of recent notes

--- app/notes-to-play.js
+++ app/notes-to-play.js
@@ -1,35 +1,63 @@
+let displayNotesIntervalId;
+let recentNotes = [];
+const amountOfRecentNotesToDisplay = 5;
+
 // function to randomly choose a note from the selected notes array every n seconds
-let displayIntervalId;
-function displayRandomNote(interval) {
-    stopDisplaying();
-    const notesToPlayDiv = document.querySelector('.notes-to-play');
+function generateRandomNotes(interval) {
+    stopDisplayingNotes();
+
+
+    const currentNoteNav = document.querySelector('.current-note');
     const selectedNotes = Array.from(document.querySelectorAll('input[name="note"]:checked'))
         .map(checkbox => checkbox.value);
-    displayIntervalId = setInterval(() => {
+
+
+    function generateRandomNote() {
         const randomIndex = Math.floor(Math.random() * selectedNotes.length);
         const randomNote = selectedNotes[randomIndex];
-        notesToPlayDiv.innerHTML = `<nav>${randomNote}<nav>`;
-    }, interval * 1000); // n seconds interval
+        displayRecentNote(randomNote);
+        currentNoteNav.innerHTML = `<nav>${randomNote}<nav>`;
+    }
+
+
+    generateRandomNote();    //generate one note when pressing the button
+
+    displayNotesIntervalId = setInterval(() => {
+        generateRandomNote(); // and then generate it every n seconds interval
+    }, interval * 1000);
 }
 
 
 // handle the submit of the interval function
-function submitRandomNote() {
+function startDisplayingNotes() {
     const intervalInput = document.getElementById('intervalInput');
     const interval = parseFloat(intervalInput.value);
     if (!isNaN(interval) && interval >= 0.3 && interval <= 10.0) {
-        displayRandomNote(interval);
+        generateRandomNotes(interval);
     } else {
         alert("Nieprawidłowe dane.");
     }
 }
 
-// function to stop displaying the notes
-function stopDisplaying() {
-    clearInterval(displayIntervalId);
-    const notesToPlayDiv = document.querySelector('.notes-to-play');
-    notesToPlayDiv.innerHTML = '';
+// handle displaying previous notes
+function displayRecentNote(note) {
+    if (recentNotes.length > amountOfRecentNotesToDisplay) {
+        recentNotes.shift();
+    }
+    recentNotes.push(note);
+    console.log(recentNotes);
 
-    const selectedNotesDiv = document.getElementById('selectedNotes');
-    selectedNotesDiv.textContent = '';
+    const previousNotesNav = document.querySelector('.previous-notes');
+    let string = "";
+
+    for (let i = 0; i < recentNotes.length - 1; i++) {
+        string += recentNotes[i] + " "
+    }
+    previousNotesNav.innerHTML = string;
+}
+
+
+// function to stop displaying the notes
+function stopDisplayingNotes() {
+    clearInterval(displayNotesIntervalId);
 }
