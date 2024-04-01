13b215efce1d3b819e4a555d52e7524c0321074f
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-01 17:12:54 +0200

    refactor

--- app/playing-notes-interval.js
+++ app/playing-notes-interval.js
@@ -2,6 +2,13 @@ let displayNotesInterval;
 let previousNotes = [];
 
 
+function getRandomNote() {
+    const selectedNotes = Array.from(document.querySelectorAll('input[name="note"]:checked'), checkbox => checkbox.value);
+    const randomIndex = Math.floor(Math.random() * selectedNotes.length);
+    return selectedNotes[randomIndex];
+}
+
+
 // handle the submit of the interval function
 function startProgram() {
     const intervalInput = document.getElementById('intervalInput');
@@ -16,10 +23,9 @@ function startProgram() {
 // fill the next notes array with 3 notes - this function is called only once - when pressing the start button 
 function initializeNextNotesArray() {
     const selectedNotes = Array.from(document.querySelectorAll('input[name="note"]:checked'), checkbox => checkbox.value); // notes selected by the user
-    const nextNotesNav = document.querySelector('.next-notes'); // the html element where the next notes are displayed
     const randomIndexes = Array.from({ length: 3 }, () => Math.floor(Math.random() * selectedNotes.length)); // generate 3 random notes
     const nextNotes = randomIndexes.map(index => selectedNotes[index]);
-    nextNotesNav.textContent = nextNotes.join(" "); // and also display it in the next-notes nav
+    updateNextNotesNav(nextNotes.join(" ")); // and also display it in the next-notes nav
     return nextNotes;
 }
 
@@ -28,29 +34,24 @@ function initializeNextNotesArray() {
 function startDisplayingNotes(interval) {
     stopDisplayingNotes() // if any notes are being displayed, stop it
 
-    const currentNoteNav = document.querySelector('.current-note');
-    const nextNotesNav = document.querySelector('.next-notes');
-    const selectedNotes = Array.from(document.querySelectorAll('input[name="note"]:checked'), checkbox => checkbox.value);
+
 
     let nextNotes = initializeNextNotesArray(); // generate initial set of notes
     swipeAllNotesLeft();
 
     // repeat every specified interval
     displayNotesInterval = setInterval(() => {
-        currentNoteNav.style.color = ''; // revert color
+        revertColorOfCurrentNote();
         swipeAllNotesLeft();
     }, interval * 1000);
 
     // helper function to display next note, and move every note 1 place left
     function swipeAllNotesLeft() {
         const currentNote = nextNotes.shift(); // take (and remove) the first note from the next notes array
-        currentNoteNav.innerHTML = `<nav>${currentNote}</nav>`; // display it as the current note
+        updateCurrentNoteNav(currentNote); // display it as the current note
         addNoteToPreviousNotes(currentNote); // and add it to previous notes array
-
-        const randomIndex = Math.floor(Math.random() * selectedNotes.length);// generate a new note 
-        nextNotes.push(selectedNotes[randomIndex]);// and add it as the last index of the next notes array
-        nextNotesNav.textContent = nextNotes.join(" "); // update next notes nav
-
+        nextNotes.push(getRandomNote());// and add it as the last index of the next notes array
+        updateNextNotesNav(nextNotes.join(" "));
     }
 }
 
@@ -75,6 +76,20 @@ function stopDisplayingNotes() {
     clearInterval(displayNotesInterval);
 }
 
+function updateCurrentNoteNav(note) {
+    const currentNoteNav = document.querySelector('.current-note');
+    currentNoteNav.innerHTML = `<nav>${note}</nav>`;
+}
+
+function updateNextNotesNav(string) {
+    const nextNotesNav = document.querySelector('.next-notes');
+    nextNotesNav.textContent = string;
+}
+
+function revertColorOfCurrentNote() {
+    const currentNoteNav = document.querySelector('.current-note');
+    currentNoteNav.style.color = '';
+}
 
 // Custom event listening when the playing note is changing
 document.addEventListener('noteChanged', function (event) {
